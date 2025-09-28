b3ce45cc8ec5b17e82e92a6a8cd7bfe89afd0a74
Chinhwinds <cchinh0201@gmail.com>
2025-09-28 20:38:58 +0700

    fix: Login error handler

--- client/web/app/auth/login/page.tsx
+++ client/web/app/auth/login/page.tsx
@@ -1,12 +1,9 @@
 import LoginForm from "../../_components/auth/LoginForm";
-import AuthGuard from "../../_components/auth/AuthGuard";
 
 export default function LoginPage() {
   return (
-    <AuthGuard requireAuth={false} redirectTo="/">
-      <div className="min-h-screen bg-gruvbox-light-bg0 dark:bg-gruvbox-dark-bg0 flex items-center justify-center p-4">
-        <LoginForm />
-      </div>
-    </AuthGuard>
+    <div className="min-h-screen bg-gruvbox-light-bg0 dark:bg-gruvbox-dark-bg0 flex items-center justify-center p-4">
+      <LoginForm />
+    </div>
   );
 }
